beae5c93c3df0c69127d6bdd488bc51cfee37286
Calvin Bruch <calvinbcoding@gmail.com>
2020-03-09 04:18:43 -0600

    added express server

--- index.js
+++ index.js
@@ -1,41 +1,49 @@
-const { graphql, buildSchema } = require('graphql')
+const express = require('express')
 
-const db = {
-    users: [
-        { id: '1', email: 'calvinb@gmail.com', name: 'Calvin' },
-        { id: '2', email: 'alexis@gmail.com', name: 'Alexis' }
-    ]
-}
+const app = express()
 
-const schema = buildSchema(`
-    type Query {
-        users: [User!]!
-    }
+app.listen(3000, () => console.log('Express server is up and listening on port 3000'))
 
-    type User {
-        id: ID!
-        email: String!
-        name: String
-        imageURL: String
-    }
-`)
 
-const rootValue = {
-    users: () => db.users
-}
+// const { graphql, buildSchema } = require('graphql')
 
-graphql(
-    schema,
-    `
-        {
-            users {
-                email
-            }
-        }
-    `,
-    rootValue
-).then(
-    res => console.dir(res, { depth: null })
-).catch(
-    console.error
-)
+// const db = {
+//     users: [
+//         { id: '1', email: 'calvinb@gmail.com', name: 'Calvin' },
+//         { id: '2', email: 'alexis@gmail.com', name: 'Alexis' }
+//     ]
+// }
+
+// const schema = buildSchema(`
+//     type Query {
+//         users: [User!]!
+//     }
+
+//     type User {
+//         id: ID!
+//         email: String!
+//         name: String
+//         imageURL: String
+//     }
+// `)
+
+// const rootValue = {
+//     users: () => db.users
+// }
+
+// graphql(
+//     schema,
+//     `
+//         {
+//             users {
+//                 id
+//                 email
+//             }
+//         }
+//     `,
+//     rootValue
+// ).then(
+//     res => console.dir(res, { depth: null })
+// ).catch(
+//     console.error
+// )
