6a9717705210061dd46b25eca2eb1764878c83d9
Calvin Bruch <calvinbcoding@gmail.com>
2020-03-09 04:28:38 -0600

    added express middleware

--- index.js
+++ index.js
@@ -1,49 +1,57 @@
 const express = require('express')
+const graphqlHTTP = require('express-graphql')
+const { buildSchema } = require('graphql')
+
+const db = {
+    users: [
+        { id: '1', email: 'calvinb@gmail.com', name: 'Calvin' },
+        { id: '2', email: 'alexis@gmail.com', name: 'Alexis' }
+    ]
+}
+
+const schema = buildSchema(`
+    type Query {
+        users: [User!]!
+    }
+
+    type User {
+        id: ID!
+        email: String!
+        name: String
+        imageURL: String
+    }
+`)
+
+const rootValue = {
+    users: () => db.users
+}
 
 const app = express()
+
+app.use('/graphql', graphqlHTTP({
+    schema,
+    rootValue,
+    graphiql: true
+}))
 
 app.listen(3000, () => console.log('Express server is up and listening on port 3000'))
 
 
 // const { graphql, buildSchema } = require('graphql')
 
-// const db = {
-//     users: [
-//         { id: '1', email: 'calvinb@gmail.com', name: 'Calvin' },
-//         { id: '2', email: 'alexis@gmail.com', name: 'Alexis' }
-//     ]
-// }
-
-// const schema = buildSchema(`
-//     type Query {
-//         users: [User!]!
-//     }
-
-//     type User {
-//         id: ID!
-//         email: String!
-//         name: String
-//         imageURL: String
-//     }
-// `)
-
-// const rootValue = {
-//     users: () => db.users
-// }
-
-// graphql(
-//     schema,
-//     `
-//         {
-//             users {
-//                 id
-//                 email
-//             }
-//         }
-//     `,
-//     rootValue
-// ).then(
-//     res => console.dir(res, { depth: null })
-// ).catch(
-//     console.error
-// )
+graphql(
+    schema,
+    `
+        {
+            users {
+                id
+                email
+            }
+        }
+    `,
+    rootValue
+).then(
+    res => console.dir(res, { depth: null })
+).catch(
+    console.error
+)
